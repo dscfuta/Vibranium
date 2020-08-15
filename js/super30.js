b400231e08e64d936045ead49d813140938e8208
Young <abiodunadebambo44@gmail.com>
2020-08-15 18:03:55 +0100

    made ad

--- js/super30.js
+++ js/super30.js
@@ -1,37 +1,20 @@
-var navBar = $('.custom-menu'),
-    navbarLinks = $('.custom-menu .nav-link');
+// the pagination
+// add event listener to the pagination box
 
-  /*====================================================*/
-  /* STICKY NAVBAR                                      */
-  /*====================================================*/
-  $(window).on('scroll', function() {
-    if ($(this).scrollTop() > 50) {
-      $(navBar).addClass('navbar-is-sticky');
-    } else {
-      $(navBar).removeClass('navbar-is-sticky');
-    }
-  });
+// get the nav element
+const paginationNav = document.getElementById('pagination');
 
-  $('.navbar-toggler').on('click', function(e) {
-    $(this).toggleClass('menu-is-expanded');
-    $('.navbar-brand').toggleClass('color-white');
-  });
+// variable
+let currentPage = 1;
 
-  $(document).on('click', '.navbar-collapse.show', function(e) {
-    if ($(e.target).is('a')) {
-      $(this).collapse('hide');
-      $('.navbar-toggler').toggleClass('menu-is-expanded');
-      // $('.Menu-Icon--Circle').css('transform', 'translateX(-50%) translateY(-50%) scale(1)');
-    }
-  });
+const handleNavigation = (e) => {
+  
+  e.preventDefault();
+}
 
-  /*====================================================*/
-  /* NAVBAR ON SCROLL EASING                            */
-  /*====================================================*/
-  $(navbarLinks).on('click', function(event) {
-    var $anchor = $(this);
-    $('html, body').stop().animate({
-      scrollTop: ($($anchor.attr('href')).offset().top - 50)
-    }, 1250, 'easeInOutExpo');
-    event.preventDefault();
-  });
+// add event listener
+// to the pagination nav
+paginationNav.addEventListener("click", handleNavigation);
+
+
+
