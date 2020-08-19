6b8af0bf5b20f093f74bb5be794ffd243c276a2f
Young <abiodunadebambo44@gmail.com>
2020-08-19 12:36:13 +0100

    render the users, getting the details from a tsv file

--- js/super30.js
+++ js/super30.js
@@ -1,20 +1,130 @@
-// the pagination
-// add event listener to the pagination box
+// functions 
+// to render the users
+const renderUsers = () => {
+  //I dowload the tsv file of the document online and converted it to json for it to be usable
 
-// get the nav element
-const paginationNav = document.getElementById('pagination');
+// get the file
+const fetchFile =async (url) => {
+  const res = await fetch(url);
 
-// variable
-let currentPage = 1;
+  const file = await res.text();
 
-const handleNavigation = (e) => {
-  
-  e.preventDefault();
+  return file;
 }
 
-// add event listener
-// to the pagination nav
-paginationNav.addEventListener("click", handleNavigation);
+
+fetchFile("./DSCfuta  Super 30 (Responses) - Form Responses 1.tsv")
+// then convert it to JSON
+.then(fileInTSV => csvToJSON(fileInTSV))
+// then pass the JSON data to the function that will use it
+.then(fileinJSON => useData(fileinJSON))
+.catch(err => console.log(err));
 
 
 
+//convert TSV to JSON
+const csvToJSON = (tsv) => {
+
+    let lines = tsv.split("\n"); //split each line into an array
+    
+    let result = [];
+  
+    let headers=lines[0].split("\t"); //get the headings
+
+
+    lines.forEach((line, index) => {
+      if (index === 0) {
+        return
+      }
+      // for each line
+      let obj = {};  //create an object
+      
+      let currentLine = line.split("\t"); //split each line with comma
+
+      // for each header, get the corresponding value and match it
+      headers.forEach((header, index) => {
+          obj[header] = currentLine[index];
+      })
+
+      // push the current object when done
+      result.push(obj)
+    })
+
+ 
+    //return result; //JavaScript object
+    return JSON.stringify(result); //JSON
+}
+
+
+const useData = (data) => {
+
+  const cardsContainer = document.getElementById('card-row');
+  
+  let cards = '';
+
+  const usersData = JSON.parse(data);
+
+  console.log(usersData)  
+
+  usersData.forEach((userData) => {
+    // console.log(userData)
+    const items = {
+      fullname: userData['Full Name'],
+      stack: userData['Which Stack are you'],
+      department: userData['Department '],
+      level: userData['Level'],
+      bio: userData['Your Bio'],
+      image_url: userData['Your Image (your image will be Published alongside your bio)']
+    }
+
+    // console.log(items)
+    cards += `<div class="my-3 col-sm-6 col-md-4 col-lg-3 card-item">
+    <div class="card hovercard">
+        <div class="cardheader"></div>
+        <div class="avatar">
+            <img src="./images/assets/team/avatar.png" alt="lead avatar" src="src/images/assets/team/avatar.png">
+        </div>
+        <div class="info">
+            <div class="title">
+                <h5>${items.fullname}</h5>
+                <p >
+                    <p class="stack">${items.stack}</p>
+                    <p class="department"> ${items.department}, ${items.level}L</p>
+                </p>
+            </div>
+            <div class="desc">${items.bio}</div>
+        </div>
+        <div class="bottom">
+            <ul class="social-list__inline mt-4">
+                <li>
+                    <a href="#" target="_blank" rel="noopener">
+                        <i class="fab fa-twitter"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" target="_blank" rel="noopener">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" target="_blank" rel="noopener">
+                        <i class="fab fa-facebook-f"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+      </div>
+    </div>`;
+  })
+
+  // insert it into the DOM
+  cardsContainer.innerHTML = cards;
+}
+
+}
+
+
+
+// event listeners
+// when the page loads, document should render the users
+document.addEventListener("DOMContentLoaded", renderUsers)
--- service-worker.js
+++ service-worker.js
@@ -1,75 +1,75 @@
-self.addEventListener('fetch', function (event) {
-    event.respondWith(caches.open('cache').then(function (cache) {
-        return cache.match(event.request).then(function (response) {
-            console.log("cache request: " + event.request.url);
-            var fetchPromise = fetch(event.request).then(function (networkResponse) {
-                console.log("fetch completed: " + event.request.url, networkResponse);
-                if (networkResponse) {
-                    console.debug("updated cached page: " + event.request.url, networkResponse);
-                    if (event.request.method === 'GET' && networkResponse.type === 'basic') {
-                        cache.put(event.request, networkResponse.clone());
-                    }
-                }
-                return networkResponse;
-            }, function (event) {
-                console.log("Error in fetch()", event);
-                event.waitUntil(
-                    caches.open('cache').then(function (cache) {
-                        return cache.addAll
-                        ([
-                            '/',
-                            '/index.html',
-                            '/index.html?homescreen=1',
-                            '/?homescreen=1',
-                            '/learn',
-                            '/learn/',
-                            '/learn/index.html',
-                            '/css/animate.css',
-                            '/css/bootstrap.min.css',
-                            '/css/ionicons.min.css',
-                            '/css/magnific-popup.css',
-                            '/css/owl.carousel.min.css',
-                            '/css/responsive.css',
-                            '/css/styles.css',
-                            '/css/swiper.min.css',
-                            '/images/assets/events/fba.png',
-                            '/images/assets/events/fbw.png',
-                            '/images/assets/events/rpj.png',
-                            '/images/assets/diversity.png',
-                            '/images/assets/logo2.png',
-                            '/images/assets/team/avatar.png',
-                            '/images/assets/technologies/android.png',
-                            '/images/assets/technologies/cloud.png',
-                            '/images/assets/technologies/mi.png',
-                            '/images/assets/technologies/web.png',
-                            '/images/assets/team/avatar.png',
-                            '/js/custom.js',
-                            '/js/data.js',
-                            '/js/firebase.config.js',
-                            '/js/vendors/bootstrap.bundle.min.js',
-                            '/js/vendors/jquery.easing.min.js',
-                            '/js/vendors/jquery.magnific-popup.min.js',
-                            '/js/vendors/jquery.min.js',
-                            '/js/vendors/owl.carousel.min.js',
-                            '/js/vendors/swiper.min.js',
-                            '/js/vendors/firebase-app.js',,
-                            '/js/vendors/firebase-storage.js',
-                            '/js/vendors/firebase-firestore.js',
-                            '/js/vendors/handlebars.min.js',
-                            'https://fonts.googleapis.com/css?family=Google+Sans:400,500,700|Material+Icons',
-                            'https://www.google-analytics.com/analytics.js',
-                            'https://use.fontawesome.com/releases/v5.2.80/css/all.css',
-                            '/service-worker.js',
-                            '/manifest.json',
-                        ]);
-                    })
-                );
-            });
-            return response || fetchPromise;
-        });
-    }));
-});
-self.addEventListener('install', function (event) {
-    self.skipWaiting();
-    console.log("Latest version installed!");
-});
+// self.addEventListener('fetch', function (event) {
+//     event.respondWith(caches.open('cache').then(function (cache) {
+//         return cache.match(event.request).then(function (response) {
+//             console.log("cache request: " + event.request.url);
+//             var fetchPromise = fetch(event.request).then(function (networkResponse) {
+//                 console.log("fetch completed: " + event.request.url, networkResponse);
+//                 if (networkResponse) {
+//                     console.debug("updated cached page: " + event.request.url, networkResponse);
+//                     if (event.request.method === 'GET' && networkResponse.type === 'basic') {
+//                         cache.put(event.request, networkResponse.clone());
+//                     }
+//                 }
+//                 return networkResponse;
+//             }, function (event) {
+//                 console.log("Error in fetch()", event);
+//                 event.waitUntil(
+//                     caches.open('cache').then(function (cache) {
+//                         return cache.addAll
+//                         ([
+//                             '/',
+//                             '/index.html',
+//                             '/index.html?homescreen=1',
+//                             '/?homescreen=1',
+//                             '/learn',
+//                             '/learn/',
+//                             '/learn/index.html',
+//                             '/css/animate.css',
+//                             '/css/bootstrap.min.css',
+//                             '/css/ionicons.min.css',
+//                             '/css/magnific-popup.css',
+//                             '/css/owl.carousel.min.css',
+//                             '/css/responsive.css',
+//                             '/css/styles.css',
+//                             '/css/swiper.min.css',
+//                             '/images/assets/events/fba.png',
+//                             '/images/assets/events/fbw.png',
+//                             '/images/assets/events/rpj.png',
+//                             '/images/assets/diversity.png',
+//                             '/images/assets/logo2.png',
+//                             '/images/assets/team/avatar.png',
+//                             '/images/assets/technologies/android.png',
+//                             '/images/assets/technologies/cloud.png',
+//                             '/images/assets/technologies/mi.png',
+//                             '/images/assets/technologies/web.png',
+//                             '/images/assets/team/avatar.png',
+//                             '/js/custom.js',
+//                             '/js/data.js',
+//                             '/js/firebase.config.js',
+//                             '/js/vendors/bootstrap.bundle.min.js',
+//                             '/js/vendors/jquery.easing.min.js',
+//                             '/js/vendors/jquery.magnific-popup.min.js',
+//                             '/js/vendors/jquery.min.js',
+//                             '/js/vendors/owl.carousel.min.js',
+//                             '/js/vendors/swiper.min.js',
+//                             '/js/vendors/firebase-app.js',,
+//                             '/js/vendors/firebase-storage.js',
+//                             '/js/vendors/firebase-firestore.js',
+//                             '/js/vendors/handlebars.min.js',
+//                             'https://fonts.googleapis.com/css?family=Google+Sans:400,500,700|Material+Icons',
+//                             'https://www.google-analytics.com/analytics.js',
+//                             'https://use.fontawesome.com/releases/v5.2.80/css/all.css',
+//                             '/service-worker.js',
+//                             '/manifest.json',
+//                         ]);
+//                     })
+//                 );
+//             });
+//             return response || fetchPromise;
+//         });
+//     }));
+// });
+// self.addEventListener('install', function (event) {
+//     self.skipWaiting();
+//     console.log("Latest version installed!");
+// });
